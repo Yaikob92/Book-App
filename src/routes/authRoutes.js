a12a416d8eb1030d434611ae6903bd923842b515
Yaikob Wasihun <wyaikob@gmail.com>
2025-11-12 10:29:06 +0300

    typo correction

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -93,7 +93,7 @@ router.post("/login", async (req, res) => {
       },
     });
   } catch (error) {
-    console.log("Error in register route", Error);
+    console.log("Error in register route", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
