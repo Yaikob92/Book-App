a72985e5f6ecd1b40a42e4a891a9290071111793
Yaikob/92 <wyaikob@gmail.com>
2025-11-13 22:09:12 +0300

    fix: typo error in register end point

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -59,7 +59,7 @@ router.post("/register", async (req, res) => {
       },
     });
   } catch (error) {
-    console.log("Error in register route", Error);
+    console.log("Error in register route", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
